597879053c9c8ff1b6d752bd0311c9ff0b403f0c
catcat20105 <thelittletalkingcat@gmail.com>
2026-08-24 13:05:00 +0800

    Add prj00.py with greeting and addition functions

---
+++ Day1/prj00.py
@@ -0,0 +1,15 @@
+def say_hi():
+    print("Hi")
+
+def say_hi_to(name):
+    print("Hi, " + name)
+
+def add_numbers(number1, number2):
+    return(number1 + number2)
+
+say_hi()
+
+say_hi_to("you")
+
+answer = add_numbers(1, 2)
+print(answer)
